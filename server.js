3f3cdfcf177f5cd60ffddb18d365c1ec565d0fc3
romiagungn <romian1394@gmail.com>
2020-02-25 14:47:44 +0700

    challenge19 page index

--- server.js
+++ server.js
@@ -6,9 +6,18 @@ var app = express();
 // const writeData = (data) => fs.writeFileSync("./data.json", JSON.stringify(data, null, 3));
 
 app.use(express.static('views'));
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
 })
 
 var server = app.listen(8081, function () {
