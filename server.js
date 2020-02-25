e04cc28d75fc17d4f6514d0e49ba3f68b949c54d
romiagungn <romian1394@gmail.com>
2020-02-25 22:26:34 +0700

    challenge19 fitur page add and views

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
 const express = require("express");
-var app = express();
-// const path = require("path");
-// const fs = require("fs");
-// const data = JSON.parse(fs.readFileSync("data.json", "utf8"));
-// const writeData = (data) => fs.writeFileSync("./data.json", JSON.stringify(data, null, 3));
+const app = express();
+const path = require("path");
+const fs = require("fs");
+const data = JSON.parse(fs.readFileSync("data.json", "utf8"));
+const writeData = (data) => fs.writeFileSync("./data.json", JSON.stringify(data, null, 3));
 
-app.use(express.static('views'));
-app.get('/index.html', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.html" );
+var bodyParser = require('body-parser')
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+   extended: false
+}))
+
+app.use(bodyParser.json())
+app.use("/", express.static(path.join(__dirname, "views")));
+
+app.get('/', function (req, res) {
+   res.render('index',{json:data})
 })
 
-app.get('/process_get', function (req, res) {
-   // Prepare output in JSON format
-   response = {
-      first_name:req.query.first_name,
-      last_name:req.query.last_name
-   };
-   console.log(response);
-   res.end(JSON.stringify(response));
-})
-
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+app.listen(3000, () => {
+   console.log(`web ini berjalan di port 3000!`);
+});
